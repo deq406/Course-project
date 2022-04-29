ae3f4d36d3e8361f109a2bab2782a1d8364abff2
Deepsel <egormartin0@gmail.com>
2022-04-30 00:58:51 +0300

    json on MainPage and modal windows

--- kursovoy/js/script.js
+++ kursovoy/js/script.js
@@ -13,56 +13,34 @@ function insertImage(jsonObj) {
   img.src = jsonObj["picture"][0]["url"][0];
   console.log(jsonObj["picture"][0]["url"][0]);
 }*/
-"use strict";
-
-function ready(fn) {
-  if (
-    document.attachEvent
-      ? document.readyState === "complete"
-      : document.readyState !== "loading"
-  ) {
-    fn();
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-  }
+function modalWindow(e) {
+  var modal = document.getElementById("myModal");
+  console.log(modal);
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = e.src;
+  captionText.innerHTML = e.alt;
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
 }
-
-ready(() => {
-  const button = document.querySelector("button");
-  button.addEventListener("click", () => {
-    // let items; //value of our items, plased in items.json file
-    // let listItems;
-    fetch("../json/pictures.json")
-      .then(function (response) {
-        if (response.status !== 200) {
-          console.log(
-            "Looks like there was a problem. Status Code: " + response.status
-          );
-          return;
-        }
-        response.json().then(function (data) {
-          // console.log(data);
-          // items = data;
-          // console.log("items:", items);
-          const listItems = data.map((item) => {
-            return `${item.url}`;
-          });
-          console.log(" new items: ", listItems);
-
-          const ul = document.createElement("ul");
-          ul.classList.add("row");
-          listItems.forEach((el) => {
-            ul.innerHTML += el;
-          });
-          const wrapper = document.querySelector(".wrapper");
-          wrapper.appendChild(ul);
-        });
-      })
-      .catch(function (err) {
-        console.log("Fetch Error :-S", err);
-      });
+/*const ApiToken = "https://run.mocky.io/v3/46f6c3a1-c444-437a-bd36-cd7a77364b93";
+const api = "https://run.mocky.io/v3/828b6f45-f5f2-4981-83a6-fc12e8d97870";
+const dai = "https://run.mocky.io/v3/2258f670-874c-4a56-89cc-dfee1895a5bd";*/
+const apiToken = "https://run.mocky.io/v3/3038094f-7d8a-4509-aa02-5842903e75ef";
+fetch(apiToken)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("data", data[0]["url"]);
+    var images = document.getElementsByClassName("image-json");
+    for (let i = 0; i < images.length; i++) {
+      images[i].src = data[i]["url"];
+    }
+  })
+  .then(function () {
+    console.log(document.querySelector(".testJson"));
   });
-
-  // const switcher = document.querySelector('input[type=checkbox]');
-  // i
-});
