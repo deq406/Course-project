62226429b5ae33ac6552061f3a341c1e22386bda
Deepsel <egormartin0@gmail.com>
2022-05-05 12:56:34 +0300

    burger menu

--- kursovoy/js/script.js
+++ kursovoy/js/script.js
@@ -44,3 +44,16 @@ fetch(apiToken)
   .then(function () {
     console.log(document.querySelector(".testJson"));
   });
+(() => {
+  window.onload = () => {
+    let header__burger = document.querySelector(".header-burger");
+    let header__menu = document.querySelector(".main-navigation");
+    console.log(header__burger, header__menu);
+    let body = document.querySelector("body");
+    header__burger.addEventListener("click", (e) => {
+      header__burger.classList.toggle("active");
+      header__menu.classList.toggle("active");
+      body.classList.toggle("lock");
+    });
+  };
+})();
